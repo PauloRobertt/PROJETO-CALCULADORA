c488ab10b8dd3628479c3789e3a4570e920fcbfc
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2023-08-03 18:54:47 -0300

    Lógica ajustada

--- index.js
+++ index.js
@@ -8,25 +8,33 @@ console.log()
 
 let valorDevido = entradaDados.question("Informe o valor devido: R$ ");
 
-let DiasVencimento = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ");
+if(valorDevido > 0){
 
-if(DiasVencimento > 15){
-    juros = 10;
-}else if(DiasVencimento == 0){
-    juros = 0;
-}else{
-    juros = 5;
-}
+    let DiasVencimento = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ");
 
-var totvJuros = (Number(juros) * Number(valorDevido)) + Number(valorDevido);
+    
+    if(DiasVencimento == 0){
+        
+        console.log("Você está em dia!")
+        
+    }else if(DiasVencimento > 0){
 
-if(DiasVencimento != 0){
-    console.log()
+        if(DiasVencimento > 15){
+            juros = 10;
+        }else{
+            juros = 5;
+        }
+    
+        var totvJuros = ((Number(juros) / 100) * Number(valorDevido)) + Number(valorDevido) ;
+        
+        console.log()
 
-    console.log("Valor original da dívida: R$ "+valorDevido);
-    console.log("Dias atrasados: "+DiasVencimento);
-    console.log("Taxa de Juros: "+juros+"%");
-    console.log("Valor total com juros: R$ "+totvJuros);
-}else{
-    console.log("Você está em dia!")
+        console.log("Valor original da dívida: R$ "+valorDevido);
+        console.log("Dias atrasados: "+DiasVencimento);
+        console.log("Taxa de Juros: "+juros+"%");
+        console.log("Valor total com juros: R$ "+totvJuros);
+    }
+
+}else if(valorDevido <= 0){
+    console.log("O valor da dívida deve ser maior que zero!")
 }
